326525794a743521ff6d9a03d5c68ce8e397aca2
Serg Gini <kornburn@gmail.com>
2021-06-14 16:06:32 +0300

    Fix some things on MacOS
    Working with airflow on old machine is a nightmare

--- airflow_ml_dags/images/airflow-train/train_model.py
+++ airflow_ml_dags/images/airflow-train/train_model.py
@@ -14,8 +14,8 @@ def train_model(input_dir: str, model_dir: str):
     X_train = pd.read_csv(os.path.join(input_dir, "X_train.csv"), index_col=0)
     y_train = pd.read_csv(os.path.join(input_dir, "y_train.csv"), index_col=0)
     model.fit(X_train, y_train)
-    os.makedirs(output_dir, exist_ok=True)
-    with open(os.path.join(output_dir, "model.pkl"), "wb") as output_file:
+    os.makedirs(model_dir, exist_ok=True)
+    with open(os.path.join(model_dir, "model.pkl"), "wb") as output_file:
         pickle.dump(model, output_file)
 
 
